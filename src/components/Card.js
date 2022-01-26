d09296861584b8695b278e954795748147ef21e5
Davina-dev <davinamr91@gmail.com>
2022-01-26 11:53:32 +0100

    front example card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,38 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div class="card" style={{ width: "18rem" }}>
-      <img class="card-img-top" src="..." alt="Card image cap" />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
+    <div
+      className="p-3 m-3 d-flex justify-content-center "
+      style={{ background: "#0A0A0A" }}
+    >
+      <div className="card p-1 m-3 " style={{ width: "16rem" }}  >
+        <img
+          className="card-img-top"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9Esa5hk256pfM68WifavLRncrp9_DxdhMufTGMUiWiU16P9XfOVeaMXRc7BS5HPYhbBo&usqp=CAU"
+          alt="Card image cap"
+        />
+        <button class="rounded-3 btn btn-secondary btn-sm opacity-75">
+          9,7
+        </button>
+
+        <div className="card-body">
+          <h5 className="card-title">🍿Card title movie </h5>
+
+          <i className="card-text">Nombre director</i>
+          <div
+            class="btn-group p-2 m-2 "
+            role="group"
+            aria-label="Basic outlined example"
+          >
+            <button type="button" class=" btn btn-outline-success">
+              Update
+            </button>
+
+            <button type="button" class="btn btn-outline-danger">
+              Delete
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
