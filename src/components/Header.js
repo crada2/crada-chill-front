bc60bfe59c0f8ce6d805f3552872d672ce5d9d15
Sol Turipe <solcis.bio@gmail.com>
2022-01-26 09:19:52 +0100

    test card

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img
+            src="/docs/5.1/assets/brand/bootstrap-logo.svg"
+            alt=""
+            width="30"
+            height="24"
+          />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
